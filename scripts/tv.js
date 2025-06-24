295236cee581f6e66acf3d50bfa27ea2cfe106ff
charlie hawley <charlie.e.hawley@gmail.com>
2025-06-24 16:39:38 +0100

    Refactor channels array in tv.js to make them less verbose and give access to mute attribute.

--- scripts/tv.js
+++ scripts/tv.js
@@ -4,24 +4,30 @@ const channel = document.querySelector('.channel');
 const pause = '⏹';
 const play = '▶';
 const channels = [
-  'https://www.youtube.com/embed/0XxNhQSj6_I?autoplay=1&loop=1&controls=0&modestbranding=1&rel=0&showinfo=0&playlist=0XxNhQSj6_I',
-  'https://www.youtube.com/embed/nEhKFxeE9fM?autoplay=1&loop=1&controls=0&modestbranding=1&rel=0&showinfo=0&playlist=nEhKFxeE9fM',
-  'https://www.youtube.com/embed/2axY5mHRBWk?autoplay=1&loop=1&controls=0&modestbranding=1&rel=0&showinfo=0&playlist=2axY5mHRBWk',
-  'https://www.youtube.com/embed/10GLMn0CieM?autoplay=1&loop=1&controls=0&modestbranding=1&rel=0&showinfo=0&playlist=10GLMn0CieM',
-  'https://www.youtube.com/embed/ZA3jw6UKv48?autoplay=1&loop=1&controls=0&modestbranding=1&rel=0&showinfo=0&playlist=ZA3jw6UKv48',
-  'https://www.youtube.com/embed/hQPM5yd1p8c?autoplay=1&loop=1&controls=0&modestbranding=1&rel=0&showinfo=0&playlist=hQPM5yd1p8c',
-  'https://www.youtube.com/embed/AmmPHHYpfTY?autoplay=1&loop=1&controls=0&modestbranding=1&rel=0&showinfo=0&playlist=AmmPHHYpfTY',
-  'https://www.youtube.com/embed/TMacoNki7Rg?autoplay=1&loop=1&controls=0&modestbranding=1&rel=0&showinfo=0&playlist=TMacoNki7Rg',
-  'https://www.youtube.com/embed/NBICGVAZgx8?autoplay=1&loop=1&controls=0&modestbranding=1&rel=0&showinfo=0&playlist=NBICGVAZgx8',
-  'https://www.youtube.com/embed/JfpDMEJoIcE?autoplay=1&loop=1&controls=0&modestbranding=1&rel=0&showinfo=0&playlist=JfpDMEJoIcE',
-  'https://www.youtube.com/embed/zyP0xPS1ydI?autoplay=1&loop=1&controls=0&modestbranding=1&rel=0&showinfo=0&playlist=zyP0xPS1ydI',
-  'https://www.youtube.com/embed/2AhufjhhK80?autoplay=1&loop=1&controls=0&modestbranding=1&rel=0&showinfo=0&playlist=2AhufjhhK80',
-  'https://www.youtube.com/embed/Hiiq9S0aW1w?autoplay=1&loop=1&controls=0&modestbranding=1&rel=0&showinfo=0&playlist=Hiiq9S0aW1w',
+  '0XxNhQSj6_I',
+  'nEhKFxeE9fM',
+  '2axY5mHRBWk',
+  '10GLMn0CieM',
+  'hQPM5yd1p8c',
+  'AmmPHHYpfTY',
+  'TMacoNki7Rg',
+  'NBICGVAZgx8',
+  'JfpDMEJoIcE',
+  'zyP0xPS1ydI',
+  '2AhufjhhK80',
+  'Hiiq9S0aW1w',
 ];
 
 document.addEventListener('DOMContentLoaded', (event) => {
   let currentChannel = Math.floor(Math.random() * channels.length);
-  channel.setAttribute('src', channels[currentChannel]);
+  channel.setAttribute(
+    'src',
+    'https://www.youtube.com/embed/' +
+      channels[currentChannel] +
+      '?autoplay=1&loop=1&controls=0&modestbranding=1&rel=0&showinfo=0&playlist=' +
+      channels[currentChannel] +
+      '&mute=1'
+  );
 });
 
 playPause.addEventListener('click', () => {
@@ -38,5 +44,13 @@ playPause.addEventListener('click', () => {
 
 switcher.addEventListener('click', () => {
   let currentChannel = Math.floor(Math.random() * channels.length);
-  channel.setAttribute('src', channels[currentChannel]);
+  channel.setAttribute(
+    'src',
+    'https://www.youtube.com/embed/' +
+      channels[currentChannel] +
+      '?autoplay=1&loop=1&controls=0&modestbranding=1&rel=0&showinfo=0&playlist=' +
+      channels[currentChannel] +
+      '&mute=1'
+  );
+  console.log(channels[currentChannel]);
 });
